bfc75d6f6075e10e7c2c49fc29e489c079b2bb1f
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-10-19 18:24:01 -0300

    feat: enable cors

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ require('dotenv/config');
 
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors');
 
 const errorHandler = require('./middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
