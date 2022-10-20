b807f48c46b0a4bd030f2dcaab356b51a9a75bd4
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-10-20 16:15:10 -0300

    refactor(AuthController): change variables

--- src/app/controllers/Auth.controller.js
+++ src/app/controllers/Auth.controller.js
@@ -8,30 +8,35 @@ const HOUR_IN_SECONDS = 3600;
 
 class AuthController {
   async login(req, res) {
-    const { value, error } = LoginSchema.validate(req.body);
+    const { value: payload, error } = LoginSchema.validate(req.body);
 
     if (error) {
       return res.status(422).json({ error: error.message });
     }
 
-    const user = await UserRepository.findByEmail(value.email);
+    const user = await UserRepository.findByEmail(payload.email);
     if (!user) {
       return res.status(400).json({ error: 'E-mail or password are invalid' });
     }
 
-    const isTheSamePassword = await Hash.compare(value.password, user.password);
+    const isTheSamePassword = await Hash.compare(payload.password, user.password);
     if (!isTheSamePassword) {
       return res.status(400).json({ error: 'E-mail or password are invalid' });
     }
 
-    const tokenPayload = {
+    const token = await Token.generate({
       iss: 'authentication-api',
       sub: user.id,
       exp: Math.floor(Date.now() / 1000) + HOUR_IN_SECONDS,
-    };
-    const token = await Token.generate(tokenPayload);
+    });
 
-    return res.status(200).json({ token });
+    const userResponse = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    return res.status(200).json({ user: userResponse, token });
   }
 }
 
