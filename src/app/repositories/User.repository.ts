1ae931bd5e1ae2448738bed080dd271302f119b9
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-11-16 15:20:32 -0300

    refactor(typescript): add types to repositories

--- src/app/repositories/User.repository.ts
+++ src/app/repositories/User.repository.ts
@@ -1,41 +1,41 @@
-const db = require('../../database');
-
-const TABLE_NAME = 'users';
+import db from '../../database';
 
 class UserRepository {
+  TABLE = 'users';
+
   async findAll() {
-    const rows = await db.query(`SELECT id, name, email FROM ${TABLE_NAME}`);
+    const rows = await db.query(`SELECT id, name, email FROM ${this.TABLE}`);
     return rows;
   }
 
-  async findById(id) {
+  async findById(id: string) {
     const [row] = await db.query(`
-      SELECT * FROM ${TABLE_NAME}
+      SELECT * FROM ${this.TABLE}
       WHERE id = $1
     `, [id]);
     return row;
   }
 
-  async findByEmail(email) {
+  async findByEmail(email: string) {
     const [row] = await db.query(`
-      SELECT * FROM ${TABLE_NAME}
+      SELECT * FROM ${this.TABLE}
       WHERE email = $1
     `, [email]);
     return row;
   }
 
-  async create({ name, email, password }) {
+  async create({ name, email, password }: { name: string; email: string; password: string }) {
     const [row] = await db.query(`
-      INSERT INTO ${TABLE_NAME} (name, email, password)
+      INSERT INTO ${this.TABLE} (name, email, password)
       VALUES ($1, $2, $3)
       RETURNING id, name, email
     `, [name, email, password]);
     return row;
   }
 
-  async update(id, { name, email }) {
+  async update(id: string, { name, email }: { name: string; email: string }) {
     const [row] = await db.query(`
-      UPDATE ${TABLE_NAME}
+      UPDATE ${this.TABLE}
       SET name = $1, email = $2
       WHERE id = $3
       RETURNING id, name, email
@@ -43,9 +43,9 @@ class UserRepository {
     return row;
   }
 
-  async updatePassword(id, { password }) {
+  async updatePassword(id: string, { password }: { password: string }) {
     const [row] = await db.query(`
-      UPDATE ${TABLE_NAME}
+      UPDATE ${this.TABLE}
       SET password = $1
       WHERE id = $2
       RETURNING id, name, email
@@ -54,4 +54,4 @@ class UserRepository {
   }
 }
 
-module.exports = new UserRepository();
+export default new UserRepository();
