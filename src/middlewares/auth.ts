9c329f0c1752710d9aab16fce85e9a1f9c3f0c86
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-11-17 10:30:22 -0300

    refactor(auth): get user and set to res.locals on middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,24 @@
 import { NextFunction, Request, Response } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
+import { validate as validateUUID } from 'uuid';
 
-import Token from '@utils/token';
+import UserRepository from '@repositories/User.repository';
+import Token from '@helpers/Token';
+
+interface Payload extends JwtPayload {
+  sub: string;
+}
+
+function isValidPayload(payload: any): asserts payload is Payload {
+  if (
+    !(typeof payload === 'object'
+      && 'sub' in payload
+      && typeof payload.sub === 'string'
+      && validateUUID(payload.sub))
+  ) {
+    throw new Error('Payload invalid');
+  }
+}
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
@@ -17,8 +35,14 @@ export default async (req: Request, res: Response, next: NextFunction) => {
 
   try {
     const payload = await Token.verify(token);
+    isValidPayload(payload);
 
-    req.userId = payload.sub;
+    const user = await UserRepository.findById(payload.sub);
+    if (!user) {
+      throw new Error('Token invalid');
+    }
+
+    res.locals.user = user;
   } catch {
     return res.status(401).json({ error: 'Token invalid' });
   }
